126dc5d36e8af25a1e855230cd108e6d71d65318
Katrinerosa <katrinerosa@gmail.com>
2026-04-30 13:50:32 +0200

    next.js Server Component commit message

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,6 @@
 export default function Contact() {
-  return (
-    <main>
-      <h1>Kontakt os her</h1>
-      <p>Du har nu lavet din første kontakt Next.js-side!</p>
-    </main>
-  );
+	return (
+		<h1>Kontakt</h1>
+		
+	);
 }
